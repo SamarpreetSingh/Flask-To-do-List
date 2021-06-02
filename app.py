f532ef9e7698668c9d4f76a526cbd27e433966b8
SamarpreetSingh <Samarpreet>
2021-06-02 13:02:32 +0530

    Added Create Functionality

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % id
+
+
+db.create_all()
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding to Database."
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        # tasks = [1, 'workout', '26 may']
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == "__main__":
